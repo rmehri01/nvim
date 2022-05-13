972b95b38d9a098eeee12c3e96117fd6436e7e19
Ryan Mehri <ryan.mehri1@gmail.com>
2022-05-13 14:36:33 -0700

    chore: format files and adjust bufferline config

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,5 +1,6 @@
 local alpha = require("alpha")
 local dashboard = require("alpha.themes.dashboard")
+
 dashboard.section.header.val = {
   "   ⣴⣶⣤⡤⠦⣤⣀⣤⠆     ⣈⣭⣿⣶⣿⣦⣼⣆          ",
   "    ⠉⠻⢿⣿⠿⣿⣿⣶⣦⠤⠄⡠⢾⣿⣿⡿⠋⠉⠉⠻⣿⣿⡛⣦       ",
@@ -13,12 +14,13 @@ dashboard.section.header.val = {
   "      ⢻⣿⣿⣄   ⠈⠻⣿⣿⣿⣷⣿⣿⣿⣿⣿⡟ ⠫⢿⣿⡆     ",
   "       ⠻⣿⣿⣿⣿⣶⣶⣾⣿⣿⣿⣿⣿⣿⣿⣿⡟⢀⣀⣤⣾⡿⠃     ",
 }
+
 dashboard.section.buttons.val = {
   dashboard.button("SPC s r", "  Recently Used Files"),
+  dashboard.button("SPC s p", "  Projects"),
   dashboard.button("SPC s t", "  Find Word"),
-  dashboard.button("SPC n", "  New File"),
-  dashboard.button("SPC f", "  Find File"),
-  dashboard.button("SPC P", "  Recent Projects"),
+  dashboard.button("SPC n", "  New File", "<cmd>ene<cr>"),
+  dashboard.button("SPC SPC", "  Find File"),
   dashboard.button("SPC C", "  Configuration"),
 }
 
