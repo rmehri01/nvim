7467ff122f1d6538964a0e72bad2897859d7f642
Ryan Mehri <ryan.mehri1@gmail.com>
2022-06-11 16:23:53 -0700

    feat: add treesitter context and update onenord

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -115,6 +115,14 @@ require("packer").startup(function(use)
   use("nvim-treesitter/nvim-treesitter-textobjects")
   use("nvim-treesitter/playground")
 
+  -- Show context of the buffer contents
+  use({
+    "nvim-treesitter/nvim-treesitter-context",
+    config = function()
+      require("treesitter-context").setup()
+    end,
+  })
+
   -- Outline window
   use({
     "stevearc/aerial.nvim",
