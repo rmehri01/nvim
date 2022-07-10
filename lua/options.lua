8a85f41bb85e0eeaa7ddada6be6eab9dd6afb7c9
Ryan Mehri <ryan.mehri1@gmail.com>
2022-07-09 19:41:28 -0700

    fix: filetype.lua is enabled by default now

--- lua/options.lua
+++ lua/options.lua
@@ -90,7 +90,3 @@ local disabled_built_ins = {
 for _, plugin in pairs(disabled_built_ins) do
   vim.g["loaded_" .. plugin] = 1
 end
-
--- Use filetype.lua
-vim.g.do_filetype_lua = 1
-vim.g.did_load_filetypes = 0
