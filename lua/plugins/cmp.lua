90819cbe49cf97711e4a23b78dd9e9256f95f487
Ryan Mehri <rmehri01@Ryans-Air.hitronhub.home>
2022-08-03 19:45:51 -0700

    feat: adjustments to cmp appearance and bindings

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -39,15 +39,22 @@ cmp.setup({
     end,
   },
   formatting = {
-    format = function(_, vim_item)
+    format = function(entry, vim_item)
       vim_item.kind = string.format("%s %s", icons[vim_item.kind], vim_item.kind)
+      vim_item.menu = ({
+        buffer = "[Buffer]",
+        nvim_lsp = "[LSP]",
+        luasnip = "[LuaSnip]",
+        nvim_lua = "[Lua]",
+        latex_symbols = "[LaTeX]",
+      })[entry.source.name]
 
       return vim_item
     end,
   },
   mapping = cmp.mapping.preset.insert({
-    ["<C-d>"] = cmp.mapping.scroll_docs(-4),
-    ["<C-f>"] = cmp.mapping.scroll_docs(4),
+    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+    ["<C-d>"] = cmp.mapping.scroll_docs(4),
     ["<C-Space>"] = cmp.mapping.complete(),
     ["<CR>"] = cmp.mapping.confirm({
       behavior = cmp.ConfirmBehavior.Replace,
