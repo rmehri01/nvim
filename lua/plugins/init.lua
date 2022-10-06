da6c9b07015ae590e4f8daa8c843cb1c06e80640
Ryan Mehri <ryan.mehri1@gmail.com>
2022-10-05 23:26:48 -0700

    feat: add fastwrap to autopairs

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -50,6 +50,15 @@ require("packer").startup(function(use)
       require("nvim-autopairs").setup({
         check_ts = true,
         enable_check_bracket_line = false,
+        fast_wrap = {
+          map = "<C-b>",
+          chars = { "{", "[", "(", '"', "'" },
+          pattern = [=[[%'%"%)%>%]%)%}%,]]=],
+          end_key = "$",
+          keys = "qwertyuiopzxcvbnmasdfghjkl",
+          highlight = "LightspeedShortcut",
+          highlight_grey = "LightspeedGreyWash",
+        },
       })
     end,
   })
